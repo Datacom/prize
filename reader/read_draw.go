487bbd65374f0bf128318b34df80c5ae64f82415
Tatiana Kudiyarova <tatiana.kudiyarova@gmail.com>
2014-09-08 16:43:44 +1200

    Parse Draw and Build DrawFile struct from uploaded file

--- reader/read_draw.go
+++ reader/read_draw.go
@@ -4,27 +4,34 @@ import (
   "fmt"
   "io"
   "bufio"
+  "strings"
 )
 
 type DrawFile struct {
-  DrawNumber int
+  DrawNumber string
   DrawDate string
   DrawTime string
   WinningNumbers string
 }
 
-func ParseDraw(f io.Reader) (d DrawFile, err error) {
-  fmt.Println("Parsing Draw File")
-  fmt.Println("File: ", f) 
+func ParseDraw(fname string, f io.Reader) (d *DrawFile, e error) {
+  fmt.Printf("Processing File: %v\n", fname)
+  draw := new(DrawFile)
+  var err error
 
-  draw := DrawFile{DrawNumber:123}
-
-  var lines []string
   scanner := bufio.NewScanner(f)
   for scanner.Scan() {
-    fmt.Println("Line: ", scanner.Text())
-    lines = append(lines, scanner.Text())
+    line       := scanner.Text()
+    line_args  := strings.Split(line, ",")
+
+    switch line_args[0] {
+      case "DrawNumber":      draw.DrawNumber = line_args[1]
+      case "DrawDate":        draw.DrawDate = line_args[1]
+      case "DrawTime":        draw.DrawTime = line_args[1]
+      case "WinningNumbers":  draw.WinningNumbers = line_args[1]
+      default: err = fmt.Errorf("[Bad Draw Error]: Unrecognized symbol %q", line_args[0])
+    }
   }
 
-  return draw, nil
+  return draw, err
 }
--- reader/read_draw_test.go
+++ reader/read_draw_test.go
@@ -1,31 +1,24 @@
 package reader
 
 import (
-  // "bufio"
-  // "io"
-  // "net"
-  // "net/http"
-  // "net/http/httptest"
   "strings"
   "testing"
-  "github.com/datacom/prize/reader"
 )
 
-func Test_Reader(t *testing.T) {
+func TestReader(t *testing.T) {
 
-  drawData := `
-    DrawNumber,1234
-    WinningNumbers, 1 2 3`
+  drawData := `DrawNumber,1234
+WinningNumbers,123`
 
   sreader := strings.NewReader(drawData)
 
-  draw, err := reader.ParseDraw(sreader)  
+  draw, err := ParseDraw(sreader)
 
   if err != nil {
     t.Error(err)
   }
 
-  if !(draw.DrawNumber == 1234) {
+  if !(draw.DrawNumber == "1234") {
     t.Error("expected 1234")
   }
 }
